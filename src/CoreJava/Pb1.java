d3db1cb993bbbbc313c77e6ad600ea8d6cc8b07c
Meghana <meghana9potta@gmail.com>
2021-12-24 03:08:15 +0530

    American guy commited

--- src/CoreJava/Pb1.java
+++ src/CoreJava/Pb1.java
@@ -7,13 +7,15 @@ public class Pb1 {
 		for(int i=1;i<=4;i++) {
 			
 			System.out.println("outer loop started");
+			System.out.println("Outer");
 			for(int j=1;j<=4;j++) {
-				System.out.println("inner loop");
+				System.out.println("inner loop started");
 				System.out.println("inner");
-				System.out.println("Outer");
 			}
 			System.out.println("outer loop finished");
+			System.out.println("Output finished");
 		}
+		System.out.println("Program is fully executed");
 	}
 
 }
